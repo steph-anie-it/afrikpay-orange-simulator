37712cc80cd13e8ba40eb862e9125443f1c8c0d2
Stephanie Takam <stephanietakam1@gmail.com>
2025-02-21 13:30:21 +0100

    test avec generation de rapport de couverture de code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,6 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
-    xdebug_set_filter(XDEBUG_FILTER_CODE_COVERAGE, XDEBUG_PATH_INCLUDE, [ __DIR__ . "/../src/" ]);
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
